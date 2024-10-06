59cd38c452532a7a76abc5794a188cf61ab663a7
mvd099 <45067416+mvd099@users.noreply.github.com>
2024-10-06 17:31:17 +0100

    Added TradingView Widgets, mobile and desktop - still breaks on some views

---
+++ TradingViewWidget.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useRef, memo } from 'react';
+
+function TradingViewWidget() {
+  const container = useRef();
+
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://s3.tradingview.com/external-embedding/embed-widget-stock-heatmap.js";
+    script.type = "text/javascript";
+    script.async = true;
+    script.innerHTML = `
+      {
+        "exchanges": [],
+        "dataSource": "SPX500",
+        "grouping": "sector",
+        "blockSize": "market_cap_basic",
+        "blockColor": "change",
+        "locale": "en",
+        "symbolUrl": "",
+        "colorTheme": "dark",
+        "hasTopBar": false,
+        "isDataSetEnabled": false,
+        "isZoomEnabled": false,
+        "hasSymbolTooltip": true,
+        "isMonoSize": false,
+        "width": "100%",
+        "height": "100%"
+      }`;
+    container.current.appendChild(script);
+  }, []);
+
+  return (
+    <div className="tradingview-widget-container" ref={container}>
+      <div className="tradingview-widget-container__widget"></div>
+      <div className="tradingview-widget-copyright">
+        <a href="https://www.tradingview.com/" rel="noopener nofollow" target="_blank">
+          <span className="blue-text"></span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default memo(TradingViewWidget);
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TradingViewWidget from './TradingViewWidget';
+
+ReactDOM.render(<TradingViewWidget />, document.getElementById('tradingview-root'));
